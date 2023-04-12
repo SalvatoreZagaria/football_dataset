4e3a4a3442d7fdd1c1b73a233cfb808e6993fbaf
SalvatoreZagaria <s.zagaria9@gmail.com>
2023-04-12 22:11:04 +0100

    SZ: Fixing logx

--- logger/__init__.py
+++ logger/__init__.py
@@ -7,7 +7,7 @@ def init_logs(logger_name: str):
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
 
-    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s - LINE: %(lineno)d')
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
